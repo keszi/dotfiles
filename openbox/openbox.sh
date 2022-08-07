0b30cf44b1b1050b7f6604c5d0d88a5df8397897
nalarian <fourbyte@protonmail.com>
2022-08-07 21:09:20 +0200

    update

--- openbox/openbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update 
-sudo apt install xorg pasystray openbox openbox-menu \
-lxappearance lxappearance-obconf tint2 plank \
-xfce4-terminal arc-theme papirus-icon-theme \
-nitrogen menu obconf numlockx picom \
-maim sxiv x11-utils rofi qt5-style-kvantum
