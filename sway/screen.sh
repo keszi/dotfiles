f15d88fa4c92b892d3600f103677003d5c45d6c4
nalarian <fourbyte@protonmail.com>
2022-08-07 15:06:46 +0200

    update

--- sway/screen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCREENSHOTS_DIR=$(xdg-user-dir PICTURES)/screenshots
-[[ ! -d "$SCREENSHOTS_DIR" ]] && mkdir -p "$SCREENSHOTS_DIR"
-
-slurp | grim -g - "$SCREENSHOTS_DIR"/$(date +%F_%T).png
