a5b8d6b876f8dad56c26c99f1d530050af2963a5
Nalarian <nalarian@protonmail.com>
2022-12-18 19:16:13 +0100

    refreshed openbox rice

--- debian11/apps.sh
+++ debian11/apps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update && sudo apt install picom nitrogen tint2 numlockx lxpolkit xserver-xorg pulseaudio -y
+sudo apt update && sudo apt install picom nitrogen tint2 menu numlockx openbox-menu qt5-style-kvantum pavucontrol alsa-utils xfce4-terminal rofi maim xserver-xorg pulseaudio lxappearance lxappearance-obconf pasystray -y
