733cba684cfa156c2887cb0e77d45f5e587dea1d
Nalarian <nalarian@protonmail.com>
2023-02-09 21:50:06 +0100

    updated for debian

--- debian11/apps.sh
+++ debian11/apps.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo apt update && sudo apt install picom nitrogen tint2 menu numlockx openbox-menu qt5-style-kvantum pavucontrol alsa-utils xfce4-terminal rofi maim xserver-xorg pulseaudio lxappearance lxappearance-obconf pasystray -y
+sudo apt update && 
+sudo apt install flameshot nitrogen tint2 \
+menu numlockx openbox-menu qt5-style-kvantum \
+pavucontrol alsa-utils xfce4-terminal \
+rofi maim xserver-xorg pulseaudio \
+lxappearance lxappearance-obconf \
+pasystray mate-polkit -y
